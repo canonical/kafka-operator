1c1bcbb9d141e9ce3a66a51d8ec8acc002cfd690
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2022-08-14 17:57:20 +0100

    chore: update config setting

---
+++ src/config.py
@@ -0,0 +1,196 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+"""Manager for handling Kafka configuration."""
+
+import logging
+from typing import Dict, List, Optional
+
+from charms.kafka.v0.kafka_snap import SNAP_CONFIG_PATH
+from ops.charm import CharmBase
+from ops.model import Unit
+
+from literals import PEER, REL_NAME, ZK
+from utils import safe_write_to_file
+
+logger = logging.getLogger(__name__)
+
+DEFAULT_CONFIG_OPTIONS = """
+log.dirs=/var/snap/kafka/common/log
+clientPort=2181
+listeners=SASL_PLAINTEXT://:9092
+sasl.enabled.mechanisms=SCRAM-SHA-512
+sasl.mechanism.inter.broker.protocol=SCRAM-SHA-512
+security.inter.broker.protocol=SASL_PLAINTEXT
+security.protocol=SASL_PLAINTEXT
+authorizer.class.name=kafka.security.authorizer.AclAuthorizer
+allow.everyone.if.no.acl.found=false
+listener.name.sasl_plaintext.sasl.enabled.mechanisms=SCRAM-SHA-512
+"""
+
+
+class KafkaConfig:
+    """Manager for handling Kafka configuration."""
+
+    def __init__(self, charm: CharmBase):
+        self.charm = charm
+        self.default_config_path = SNAP_CONFIG_PATH
+        self.properties_filepath = f"{self.default_config_path}/server.properties"
+        self.jaas_filepath = f"{self.default_config_path}/kafka-jaas.cfg"
+
+    @property
+    def sync_password(self) -> Optional[str]:
+        """Returns charm-set sync_password for server-server auth between brokers."""
+        return self.charm.model.get_relation(PEER).data[self.charm.app].get("sync_password", None)
+
+    @property
+    def zookeeper_config(self) -> Dict[str, str]:
+        """Checks the zookeeper relations for data necessary to connect to ZooKeeper.
+
+        Returns:
+            Dict with zookeeper username, password, endpoints, chroot and uris
+        """
+        zookeeper_config = {}
+        for relation in self.charm.model.relations[ZK]:
+            zk_keys = ["username", "password", "endpoints", "chroot", "uris"]
+            missing_config = any(
+                relation.data[relation.app].get(key, None) is None for key in zk_keys
+            )
+
+            if missing_config:
+                continue
+
+            zookeeper_config.update(relation.data[relation.app])
+            break
+
+        if zookeeper_config:
+            sorted_uris = sorted(
+                zookeeper_config["uris"].replace(zookeeper_config["chroot"], "").split(",")
+            )
+            sorted_uris[-1] = sorted_uris[-1] + zookeeper_config["chroot"]
+            zookeeper_config["connect"] = ",".join(sorted_uris)
+
+        return zookeeper_config
+
+    @property
+    def zookeeper_connected(self) -> bool:
+        if self.zookeeper_config.get("connect", None):
+            return True
+
+        return False
+
+    @property
+    def extra_args(self) -> List[str]:
+        return [f"-Djava.security.auth.login.config={self.jaas_filepath}"]
+
+    @property
+    def bootstrap_server(self) -> List[str]:
+        units: List[Unit] = list(
+            set([self.charm.unit] + list(self.charm.model.get_relation(PEER).units))
+        )
+        hosts = [
+            self.charm.model.get_relation(PEER).data[unit].get("private-address") for unit in units
+        ]
+        return [f"{host}:9092" for host in hosts]
+
+    @property
+    def default_replication_properties(self) -> List[str]:
+        """Builds replication-related properties based on the expected app size.
+
+        Returns:
+            List of properties to be set
+        """
+        replication_factor = min([3, self.charm.app.planned_units()])
+        min_isr = max([1, replication_factor - 1])
+
+        return [
+            f"default.replication.factor={replication_factor}",
+            f"num.partitions={replication_factor}",
+            f"transaction.state.log.replication.factor={replication_factor}",
+            f"offsets.topic.replication.factor={replication_factor}",
+            f"min.insync.replicas={min_isr}",
+            f"transaction.state.log.min.isr={min_isr}",
+        ]
+
+    @property
+    def auth_properties(self) -> List[str]:
+        """Builds properties necessary for inter-broker authorization through ZooKeeper.
+
+        Returns:
+            List of properties to be set
+        """
+        broker_id = self.charm.unit.name.split("/")[1]
+        host = (
+            self.charm.model.get_relation(PEER).data[self.charm.unit].get("private-address", None)
+        )
+        return [
+            f"broker.id={broker_id}",
+            f"advertised.listeners=SASL_PLAINTEXT://{host}:9092",
+            f'zookeeper.connect={self.zookeeper_config["connect"]}',
+            f'listener.name.sasl_plaintext.scram-sha-512.sasl.jaas.config=org.apache.kafka.common.security.scram.ScramLoginModule required username="sync" password="{self.sync_password}";',
+        ]
+
+    @property
+    def super_users(self) -> str:
+        """Builds all users with super/admin permissions for the cluster.
+
+        Returns:
+            Semi-colon delimited list of super users
+        """
+        super_users = ["sync"]
+        for relation in self.charm.model.relations[REL_NAME]:
+            extra_user_roles = relation.data[relation.app].get("extra-user-roles", "")
+            username = (
+                self.charm.model.get_relation(PEER)
+                .data[self.charm.app]
+                .get(f"relation-{relation.id}", None)
+            )
+            if "admin" in extra_user_roles and username is not None:
+                super_users.append(f"relation-{relation.id}")
+
+        super_users_arg = [f"User:{user}" for user in super_users]
+
+        return ";".join(super_users_arg)
+
+    @property
+    def server_properties(self) -> List[str]:
+        return (
+            [
+                f"offsets.retention.minutes={self.charm.config['offsets-retention-minutes']}",
+                f"log.retention.hours={self.charm.config['log-retention-hours']}",
+                f"auto.create.topics={self.charm.config['auto-create-topics']}",
+                f"super.users={self.super_users}",
+            ]
+            + self.default_replication_properties
+            + self.auth_properties
+            + DEFAULT_CONFIG_OPTIONS.split("\n")
+        )
+
+    def set_jaas_config(self) -> None:
+        """Sets the Kafka JAAS config using zookeeper relation data."""
+        jaas_config = f"""
+            Client {{
+                org.apache.zookeeper.server.auth.DigestLoginModule required
+                username="{self.zookeeper_config['username']}"
+                password="{self.zookeeper_config['password']}";
+            }};
+        """
+        safe_write_to_file(content=jaas_config, path=self.jaas_filepath, mode="w")
+
+    def set_server_properties(self) -> None:
+        """Sets all kafka config properties to the server.properties path."""
+        safe_write_to_file(
+            content="\n".join(self.server_properties),
+            path=self.properties_filepath,
+            mode="w",
+        )
+
+    def set_kafka_opts(self) -> None:
+        """Sets the env-vars needed for SASL auth to /etc/environment on the unit."""
+        opts_string = " ".join(self.extra_args)
+        safe_write_to_file(
+            content=f"KAFKA_OPTS={opts_string}",
+            path="/etc/environment",
+            mode="a",
+        )
