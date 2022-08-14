599b66f1d930d6c893f7643ba52be53fc09e841e
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2022-08-14 17:57:39 +0100

    feat: add literals for global vars

---
+++ src/literals.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+# Copyright 2022 Canonical Ltd.
+# See LICENSE file for licensing details.
+
+CHARM_KEY = "kafka"
+PEER = "cluster"
+ZK = "zookeeper"
+REL_NAME = "kafka-client"
