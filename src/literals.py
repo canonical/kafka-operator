77222627f4d3666217eebc57813c4c878bb47a7b
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2022-08-14 18:47:52 +0100

    docs: update literals docstrings

--- src/literals.py
+++ src/literals.py
@@ -2,6 +2,8 @@
 # Copyright 2022 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+"""Collection of globals common to the KafkaCharm."""
+
 CHARM_KEY = "kafka"
 PEER = "cluster"
 ZK = "zookeeper"
