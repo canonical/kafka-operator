26da275595935c904dc4eea35068e9c9cdfe364a
Raul Zamora <raul.zamora@canonical.com>
2022-10-11 18:33:22 +0200

    call on leader only

--- src/charm.py
+++ src/charm.py
@@ -177,7 +177,7 @@ class KafkaCharm(CharmBase):
 
             self.on[self.restart.name].acquire_lock.emit()
 
-        if self.model.relations.get(REL_NAME, None):
+        if self.model.relations.get(REL_NAME, None) and self.unit.is_leader():
             self.provider.update_connection_info()
 
     def _restart(self, event: EventBase) -> None:
