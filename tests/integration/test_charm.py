6bbf5c8778fcab9003ea7b059460f2155c31d548
Paolo Sottovia <paolo.sottovia@canonical.com>
2023-01-27 13:14:53 +0000

    Try to fix CI

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -24,6 +24,14 @@ DUMMY_NAME = "app"
 
 
 @pytest.mark.abort_on_fail
+async def test_build_and_deploy_app(ops_test: OpsTest, app_charm: PosixPath):
+    await asyncio.gather(
+        ops_test.model.deploy(app_charm, application_name=DUMMY_NAME, num_units=1, series="jammy"),
+    )
+    await ops_test.model.wait_for_idle(apps=[DUMMY_NAME])
+
+
+@pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest):
     kafka_charm = await ops_test.build_charm(".")
     await asyncio.gather(
