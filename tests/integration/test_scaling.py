35df0e8a9db55cd8b2a7c0f473bedebe4aae1c65
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-03-09 01:26:53 +0000

    fix/test: add fastforward to scaling deploy

--- tests/integration/test_scaling.py
+++ tests/integration/test_scaling.py
@@ -31,11 +31,13 @@ async def test_kafka_simple_scale_up(ops_test: OpsTest, kafka_charm):
         ),
         ops_test.model.deploy(kafka_charm, application_name=APP_NAME, num_units=1, series="jammy"),
     )
-    await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK], idle_period=30, timeout=1800)
-    await ops_test.model.add_relation(APP_NAME, ZK)
-    await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK], idle_period=30, timeout=600)
-    assert ops_test.model.applications[ZK].status == "active"
-    assert ops_test.model.applications[APP_NAME].status == "active"
+
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK], idle_period=30, timeout=1800)
+        await ops_test.model.add_relation(APP_NAME, ZK)
+        await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK], idle_period=30, timeout=600)
+        assert ops_test.model.applications[ZK].status == "active"
+        assert ops_test.model.applications[APP_NAME].status == "active"
 
     await ops_test.model.applications[APP_NAME].add_units(count=2)
     await ops_test.model.block_until(lambda: len(ops_test.model.applications[APP_NAME].units) == 3)
