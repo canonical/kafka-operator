965916bc41e8dd5fc4ab8b8bd5f7a6b0ab7c5e2b
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-03-14 21:30:59 +0000

    fix/test: fastforward provider tls relation

--- tests/integration/test_provider.py
+++ tests/integration/test_provider.py
@@ -229,14 +229,15 @@ async def test_connection_updated_on_tls_enabled(ops_test: OpsTest, app_charm):
     await ops_test.model.deploy(TLS_NAME, channel="beta", config=tls_config, series="jammy")
     await ops_test.model.add_relation(TLS_NAME, ZK)
 
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, ZK, TLS_NAME, DUMMY_NAME_1], timeout=1000, idle_period=40
-    )
-    await ops_test.model.add_relation(TLS_NAME, f"{APP_NAME}:{REL_NAME_CERTIFICATES}")
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME, ZK, TLS_NAME, DUMMY_NAME_1], timeout=1000, idle_period=40
+        )
+        await ops_test.model.add_relation(TLS_NAME, f"{APP_NAME}:{REL_NAME_CERTIFICATES}")
 
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, ZK, TLS_NAME, DUMMY_NAME_1], timeout=1000, idle_period=40
-    )
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME, ZK, TLS_NAME, DUMMY_NAME_1], timeout=1000, idle_period=40
+        )
 
     assert ops_test.model.applications[APP_NAME].status == "active"
     assert ops_test.model.applications[ZK].status == "active"
