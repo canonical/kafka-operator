f2219cc4069850cdad04732aeaa98e3c5417234c
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-04-18 16:29:56 +0100

    test: add single machine zk/kafka int test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -30,6 +30,40 @@ DUMMY_NAME = "app"
 
 
 @pytest.mark.abort_on_fail
+async def test_build_and_deploy_same_machine(ops_test: OpsTest, kafka_charm):
+    # deploying 1 machine
+    await ops_test.model.add_machine(series="jammy")
+    machines = await ops_test.model.get_machines()
+
+    # deploying both kafka + zk to same machine
+    await asyncio.gather(
+        ops_test.model.deploy(
+            ZK_NAME,
+            channel="edge",
+            application_name=ZK_NAME,
+            num_units=1,
+            series="jammy",
+            to=machines[0],
+        ),
+        ops_test.model.deploy(
+            kafka_charm, application_name=APP_NAME, num_units=1, series="jammy", to=machines[0]
+        ),
+    )
+    await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME], idle_period=30, timeout=3600)
+    assert ops_test.model.applications[APP_NAME].status == "blocked"
+    assert ops_test.model.applications[ZK_NAME].status == "active"
+
+    await ops_test.model.add_relation(APP_NAME, ZK_NAME)
+    async with ops_test.fast_forward():
+        await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME], idle_period=30)
+        assert ops_test.model.applications[APP_NAME].status == "active"
+        assert ops_test.model.applications[ZK_NAME].status == "active"
+
+    # teardown and cleanup for remaining tests
+    await ops_test.model.reset(force=True)
+
+
+@pytest.mark.abort_on_fail
 async def test_build_and_deploy(ops_test: OpsTest, kafka_charm):
     await asyncio.gather(
         ops_test.model.deploy(
