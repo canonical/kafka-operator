3365a80180d21509d36f06a4c766dcb75799ce37
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2023-09-04 19:02:02 +0100

    chore: bump snap rev

--- src/literals.py
+++ src/literals.py
@@ -13,7 +13,7 @@ from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, StatusBase
 
 CHARM_KEY = "kafka"
 SNAP_NAME = "charmed-kafka"
-CHARMED_KAFKA_SNAP_REVISION = 16
+CHARMED_KAFKA_SNAP_REVISION = 19
 
 PEER = "cluster"
 ZK = "zookeeper"
