ac5b21d69771054beb9332472e83f403d72d6f31
Raul Zamora <raul.zamora@canonical.com>
2023-09-25 15:23:06 +0200

    remove unused code

--- tests/integration/ha/test_ha.py
+++ tests/integration/ha/test_ha.py
@@ -85,11 +85,6 @@ async def test_build_and_deploy(ops_test: OpsTest, kafka_charm, app_charm):
     assert ops_test.model.applications[APP_NAME].status == "active"
     assert ops_test.model.applications[DUMMY_NAME].status == "active"
 
-    await ops_test.model.applications[APP_NAME].add_units(count=2)
-    await ops_test.model.wait_for_idle(
-        apps=[APP_NAME], status="active", timeout=600, idle_period=120, wait_for_exact_units=3
-    )
-
 
 async def test_replicated_events(ops_test: OpsTest):
     await ops_test.model.applications[APP_NAME].add_units(count=2)
