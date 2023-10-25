1426c8d301e5c019174dcb860acae7fd823be6af
deusebio <enrico.deusebio@canonical.com>
2023-10-25 22:01:45 +0200

    [DPE-2704] Integrate Kafka built from source (#144)

--- src/literals.py
+++ src/literals.py
@@ -13,7 +13,7 @@ from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, StatusBase
 
 CHARM_KEY = "kafka"
 SNAP_NAME = "charmed-kafka"
-CHARMED_KAFKA_SNAP_REVISION = 19
+CHARMED_KAFKA_SNAP_REVISION = 21
 
 PEER = "cluster"
 ZK = "zookeeper"
