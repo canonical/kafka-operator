5a432405513406a31d79630739c0290da6f0ce16
Enrico Deusebio <enrico.deusebio@canonical.com>
2024-02-26 22:24:13 +0100

    fix linting

--- tests/integration/test_provider.py
+++ tests/integration/test_provider.py
@@ -37,7 +37,11 @@ async def test_deploy_charms_relate_active(
     """Test deploy and relate operations."""
     await asyncio.gather(
         ops_test.model.deploy(
-            "zookeeper", channel="candidate", application_name="zookeeper", num_units=3, series="jammy"
+            "zookeeper",
+            channel="candidate",
+            application_name="zookeeper",
+            num_units=3,
+            series="jammy",
         ),
         ops_test.model.deploy(kafka_charm, application_name=APP_NAME, num_units=1, series="jammy"),
         ops_test.model.deploy(
