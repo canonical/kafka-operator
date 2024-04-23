916a9529170a6b7d9685309cd38f993bcdda46a5
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-04-23 15:06:45 +0100

    chore: working charm int-test

--- tests/integration/test_charm.py
+++ tests/integration/test_charm.py
@@ -95,11 +95,10 @@ async def test_build_and_deploy(ops_test: OpsTest, kafka_charm):
     assert ops_test.model.applications[ZK_NAME].status == "active"
 
     await ops_test.model.add_relation(APP_NAME, ZK_NAME)
-    async with ops_test.fast_forward():
-        await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME])
-
-    assert ops_test.model.applications[APP_NAME].status == "active"
-    assert ops_test.model.applications[ZK_NAME].status == "active"
+    async with ops_test.fast_forward(fast_interval="60s"):
+        await ops_test.model.wait_for_idle(
+            apps=[APP_NAME, ZK_NAME], idle_period=30, status="active"
+        )
 
 
 @pytest.mark.abort_on_fail
@@ -118,7 +117,7 @@ async def test_remove_zk_relation_relate(ops_test: OpsTest):
     assert ops_test.model.applications[ZK_NAME].status == "active"
 
     await ops_test.model.add_relation(APP_NAME, ZK_NAME)
-    async with ops_test.fast_forward():
+    async with ops_test.fast_forward(fast_interval="60s"):
         await ops_test.model.wait_for_idle(
             apps=[APP_NAME, ZK_NAME], status="active", idle_period=30, timeout=1000
         )
