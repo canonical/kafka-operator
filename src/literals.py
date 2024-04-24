35870b5426c4d23fe25745c2d4aade478eec555b
Alex Batisse <alex.batisse@canonical.com>
2024-04-24 16:03:53 +0200

    chore: Bump kafka snap to latest 3/edge revision

--- src/literals.py
+++ src/literals.py
@@ -12,7 +12,7 @@ from ops.model import ActiveStatus, BlockedStatus, MaintenanceStatus, StatusBase
 
 CHARM_KEY = "kafka"
 SNAP_NAME = "charmed-kafka"
-CHARMED_KAFKA_SNAP_REVISION = 34
+CHARMED_KAFKA_SNAP_REVISION = 35
 CONTAINER = "kafka"
 
 PEER = "cluster"
