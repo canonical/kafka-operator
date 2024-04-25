c0d2ac88fa505be98a55924889486edfc08b60b3
Alex Batisse <alex.batisse@canonical.com>
2024-04-25 16:50:43 +0200

    WIP: Rework condition in storage added hook

--- src/charm.py
+++ src/charm.py
@@ -250,17 +250,20 @@ class KafkaCharm(TypedCharmBase[CharmConfig]):
         # new dirs won't be used until topic partitions are assigned to it
         # either automatically for new topics, or manually for existing
         # set status only for running services, not on startup
+        # if self.workload.active():
+        #     self._set_status(Status.ADDED_STORAGE)
+        #     self.workload.exec(f"chown -R {USER}:{GROUP} {self.workload.paths.data_path}")
+        #     self.workload.exec(f"chmod -R 750 {self.workload.paths.data_path}")
+        #     self._on_config_changed(event)
+        # else:
+        self.workload.exec(f"chmod -R 750 {self.workload.paths.data_path}")
+        self.workload.exec(f"chown -R {USER}:{GROUP} {self.workload.paths.data_path}")
+        self.workload.exec(
+            f"""find {self.workload.paths.data_path} -type f -name "meta.properties" -delete || true"""
+        )
         if self.workload.active():
             self._set_status(Status.ADDED_STORAGE)
-            self.workload.exec(f"chown -R {USER}:{GROUP} {self.workload.paths.data_path}")
-            self.workload.exec(f"chmod -R 750 {self.workload.paths.data_path}")
             self._on_config_changed(event)
-        else:
-            self.workload.exec(f"chmod -R 750 {self.workload.paths.data_path}")
-            self.workload.exec(f"chown -R {USER}:{GROUP} {self.workload.paths.data_path}")
-            self.workload.exec(
-                f"""find {self.workload.paths.data_path} -type f -name "meta.properties" -delete || true"""
-            )
 
     def _on_storage_detaching(self, event: StorageDetachingEvent) -> None:
         """Handler for `storage_detaching` events."""
