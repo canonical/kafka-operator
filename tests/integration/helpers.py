7713ee58f8fab0a490382466d3d86583e5e965b3
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-04-26 02:23:27 +0100

    temp: debugging ci

--- tests/integration/helpers.py
+++ tests/integration/helpers.py
@@ -139,6 +139,7 @@ def check_tls(ip: str, port: int) -> bool:
             shell=True,
             universal_newlines=True,
         )
+        logger.info(f"{result=}")
         # FIXME: The server cannot be validated, we would need to try to connect using the CA
         # from self-signed certificates. This is indication enough that the server is sending a
         # self-signed key.
