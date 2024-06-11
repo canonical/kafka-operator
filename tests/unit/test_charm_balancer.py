b55d412dedb767ba0ebd4796223510022cd1b23a
Alex Batisse <alex.batisse@canonical.com>
2024-06-11 11:42:02 +0200

    tests: Introduce scenario based testing

--- tests/unit/test_charm_balancer.py
+++ tests/unit/test_charm_balancer.py
@@ -2,56 +2,93 @@
 # Copyright 2024 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import json
 import logging
 from pathlib import Path
 from unittest.mock import patch
 
 import pytest
 import yaml
-from ops.testing import Harness
-from src.literals import Status
+from scenario import Context, Relation, State
 
 from charm import KafkaCharm
-from literals import (
-    BALANCER,
-    CONTAINER,
-    SUBSTRATE,
-)
+from literals import BALANCER, BALANCER_SERVICE, BROKER, Status
 
 pytestmark = pytest.mark.balancer
 
 logger = logging.getLogger(__name__)
 
 
-CONFIG = str(yaml.safe_load(Path("./config.yaml").read_text()))
-ACTIONS = str(yaml.safe_load(Path("./actions.yaml").read_text()))
-METADATA = str(yaml.safe_load(Path("./metadata.yaml").read_text()))
+CONFIG = yaml.safe_load(Path("./config.yaml").read_text())
+ACTIONS = yaml.safe_load(Path("./actions.yaml").read_text())
+METADATA = yaml.safe_load(Path("./metadata.yaml").read_text())
 
 
-@pytest.fixture
-def harness() -> Harness:
-    harness = Harness(KafkaCharm, meta=METADATA, actions=ACTIONS, config=CONFIG)
-
-    if SUBSTRATE == "k8s":
-        harness.set_can_connect(CONTAINER, True)
-
-    harness._update_config({"role": "balancer"})
-    harness.begin()
-    return harness
+@pytest.fixture()
+def charm_configuration():
+    """Enable direct mutation on configuration dict."""
+    return json.loads(json.dumps(CONFIG))
 
 
-def test_start_with_correct_role(harness: Harness):
-    assert harness.charm.config["role"] == BALANCER
+def test_install_blocks_snap_install_failure(charm_configuration):
+    # Given
+    charm_configuration["options"]["role"]["default"] = "balancer"
+    ctx = Context(
+        KafkaCharm,
+        meta=METADATA,
+        config=charm_configuration,
+        actions=ACTIONS,
+    )
+    state_in = State()
 
-
-def test_install_blocks_snap_install_failure(harness: Harness):
-    """Checks unit goes to BlockedStatus after snap failure on install hook."""
+    # When
     with patch("workload.Workload.install", return_value=False):
-        harness.charm.on.install.emit()
-        assert harness.charm.unit.status == Status.SNAP_NOT_INSTALLED.value.status
+        state_out = ctx.run("install", state_in)
+
+    # Then
+    assert state_out.unit_status == Status.SNAP_NOT_INSTALLED.value.status
 
 
-def test_ready_to_start_not_implemented(harness: Harness):
-    with patch("workload.Workload.write"):
-        harness.charm.on.start.emit()
-    assert harness.charm.unit.status == Status.NOT_IMPLEMENTED.value.status
+def test_ready_to_start_not_implemented(charm_configuration):
+    # Given
+    charm_configuration["options"]["role"]["default"] = "balancer"
+    ctx = Context(
+        KafkaCharm,
+        meta=METADATA,
+        config=charm_configuration,
+        actions=ACTIONS,
+    )
+    state_in = State()
+
+    # When
+    with patch("workload.Workload.write"), patch("workload.BalancerWorkload.start"):
+        state_out = ctx.run("start", state_in)
+
+    # Then
+    assert state_out.unit_status == Status.NOT_IMPLEMENTED.value.status
+
+
+def test_secrets_requested_by_balancer_on_relation_creation(charm_configuration):
+    # Given
+    charm_configuration["options"]["role"]["default"] = "balancer"
+    ctx = Context(
+        KafkaCharm,
+        meta=METADATA,
+        config=charm_configuration,
+        actions=ACTIONS,
+    )
+    relation = Relation(
+        interface=BALANCER.value,
+        endpoint=BALANCER_SERVICE,
+        remote_app_name=BROKER.value,
+    )
+    state_in = State(leader=True, relations=[relation])
+
+    # When
+    state_out = ctx.run(relation.created_event, state_in)
+
+    # Then
+    assert (
+        json.loads(state_out.relations[0].local_app_data["requested-secrets"])
+        == BALANCER.requested_secrets
+    )
