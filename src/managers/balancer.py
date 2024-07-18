f1ccec15ebb19f210db5f9651c0f5a5a2ae6bac2
Alex Batisse <alex.batisse@canonical.com>
2024-07-18 15:36:08 +0200

    fix: Use internal uris for balancer topics in single app

--- src/managers/balancer.py
+++ src/managers/balancer.py
@@ -8,7 +8,7 @@ import json
 import logging
 from typing import TYPE_CHECKING
 
-from literals import BALANCER, BALANCER_TOPICS, STORAGE
+from literals import BALANCER, BALANCER_TOPICS, BROKER, STORAGE
 
 if TYPE_CHECKING:
     from charm import KafkaCharm
@@ -49,15 +49,22 @@ class BalancerManager:
 
     def create_internal_topics(self) -> None:
         """Create Cruise Control topics."""
+        if self.charm.state.runs_broker:
+            property_file = f'{BROKER.paths["CONF"]}/client.properties'
+            bootstrap_servers = self.charm.state.internal_bootstrap_server
+        else:
+            property_file = f'{BALANCER.paths["CONF"]}/cruisecontrol.properties'
+            bootstrap_servers = self.charm.state.balancer.broker_uris
+
         for topic in BALANCER_TOPICS:
             if topic not in self.dependent.workload.run_bin_command(
                 "topics",
                 [
                     "--list",
                     "--bootstrap-server",
-                    f"{self.charm.state.balancer.broker_uris}",
+                    bootstrap_servers,
                     "--command-config",
-                    f'{BALANCER.paths["CONF"]}/cruisecontrol.properties',
+                    property_file,
                 ],
             ):
                 self.dependent.workload.run_bin_command(
@@ -67,9 +74,9 @@ class BalancerManager:
                         "--topic",
                         topic,
                         "--bootstrap-server",
-                        f"{self.charm.state.balancer.broker_uris}",
+                        bootstrap_servers,
                         "--command-config",
-                        f'{BALANCER.paths["CONF"]}/cruisecontrol.properties',
+                        property_file,
                     ],
                 )
                 logger.info(f"Created topic {topic}")
