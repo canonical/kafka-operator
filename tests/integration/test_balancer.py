04705a4ad2b187de3d3eeedeb7ff08499f282dee
Alex Batisse <alex.batisse@canonical.com>
2024-07-19 10:55:07 +0200

    tests: Remove duplicate wait operation in balancer test

--- tests/integration/test_balancer.py
+++ tests/integration/test_balancer.py
@@ -54,10 +54,9 @@ async def test_relate_not_enough_brokers(ops_test: OpsTest):
 @pytest.mark.abort_on_fail
 async def test_minimum_brokers_balancer_starts(ops_test: OpsTest):
     await ops_test.model.applications[APP_NAME].add_units(count=2)
-    await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK_NAME], idle_period=30, status="active")
     await ops_test.model.block_until(lambda: len(ops_test.model.applications[APP_NAME].units) == 4)
     await ops_test.model.wait_for_idle(
-        apps=[APP_NAME], status="active", timeout=1800, idle_period=30
+        apps=[APP_NAME, ZK_NAME], status="active", timeout=1800, idle_period=30
     )
     assert balancer_is_running(model_full_name=ops_test.model_full_name, app_name=APP_NAME)
     assert balancer_is_secure(ops_test, app_name=APP_NAME)
