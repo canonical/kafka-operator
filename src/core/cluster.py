1d66f9b5792cb09f43a10bc7f3981cd16fa73dbd
Marc Oppenheimer <marcaoppenheimer@gmail.com>
2024-09-30 18:08:09 +0100

    refactor: assume single peer-cluster relation

--- src/core/cluster.py
+++ src/core/cluster.py
@@ -124,9 +124,9 @@ class ClusterState(Object):
         return set(self.model.relations[REL_NAME])
 
     @property
-    def peer_cluster_orchestrator_relations(self) -> set[Relation]:
-        """The `peer-cluster-orchestrator` relations that this charm is providing."""
-        return set(self.model.relations[PEER_CLUSTER_ORCHESTRATOR_RELATION])
+    def peer_cluster_orchestrator_relation(self) -> Relation | None:
+        """The `peer-cluster-orchestrator` relation that this charm is providing."""
+        return self.model.get_relation(PEER_CLUSTER_ORCHESTRATOR_RELATION)
 
     @property
     def peer_cluster_relation(self) -> Relation | None:
@@ -134,27 +134,33 @@ class ClusterState(Object):
         return self.model.get_relation(PEER_CLUSTER_RELATION)
 
     @property
-    def peer_clusters(self) -> set[PeerCluster]:
-        """The state for all related `peer-cluster` applications that this charm is providing for."""
-        peer_clusters = set()
-        balancer_kwargs: dict[str, Any] = {
-            "balancer_username": self.cluster.balancer_username,
-            "balancer_password": self.cluster.balancer_password,
-            "balancer_uris": self.cluster.balancer_uris,
-        }
-        for relation in self.peer_cluster_orchestrator_relations:
-            if not relation.app or not self.runs_balancer:
-                continue
+    def peer_cluster_orchestrator(self) -> PeerCluster:
+        """The state for the related `peer-cluster-orchestrator` application that this charm is requiring from."""
+        balancer_kwargs: dict[str, Any] = (
+            {
+                "balancer_username": self.cluster.balancer_username,
+                "balancer_password": self.cluster.balancer_password,
+                "balancer_uris": self.cluster.balancer_uris,
+            }
+            if self.runs_balancer
+            else {}
+        )
 
-            peer_clusters.add(
-                PeerCluster(
-                    relation=relation,
-                    data_interface=PeerClusterOrchestratorData(self.model, relation.name),
-                    **balancer_kwargs,
-                )
-            )
+        return PeerCluster(
+            relation=self.peer_cluster_relation,
+            data_interface=PeerClusterData(self.model, PEER_CLUSTER_RELATION),
+            **balancer_kwargs,
+        )
 
-        return peer_clusters
+    @property
+    def peer_cluster(self) -> PeerCluster:
+        """The state for the related `peer-cluster` application that this charm is providing to."""
+        return PeerCluster(
+            relation=self.peer_cluster_orchestrator_relation,
+            data_interface=PeerClusterOrchestratorData(
+                self.model, PEER_CLUSTER_ORCHESTRATOR_RELATION
+            ),
+        )
 
     # FIXME: will need renaming once we use Kraft as the orchestrator
     # uses the 'already there' BALANCER username now
