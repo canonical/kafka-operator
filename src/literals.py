cc5c90ae5740482bf2bfc72d53aadd3d285efa78
Iman Enami <iman.enami@canonical.com>
2024-12-02 16:52:36 +0400

    fix: bump kafka dependency version

--- src/literals.py
+++ src/literals.py
@@ -292,6 +292,6 @@ DEPENDENCIES = {
         "dependencies": {"zookeeper": ">3.6"},
         "name": "kafka",
         "upgrade_supported": "^3",  # zk support removed in 4.0
-        "version": "3.6.1",
+        "version": "3.9.0",
     },
 }
