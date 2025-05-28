7df340853f901139cad8c11d41399aca70ee0fbc
Iman Enami <iman.enami@canonical.com>
2025-05-28 18:17:03 +0400

    cicd: add provider int. tests

--- tests/integration/test_provider.py
+++ tests/integration/test_provider.py
@@ -8,12 +8,13 @@ import logging
 import pytest
 from pytest_operator.plugin import OpsTest
 
+from literals import INTERNAL_USERS, SECURITY_PROTOCOL_PORTS
+
 from .helpers import (
-    ZK,
     check_user,
     deploy_cluster,
+    get_address,
     get_client_usernames,
-    get_kafka_zk_relation_data,
     get_provider_data,
     load_acls,
     load_super_users,
@@ -36,6 +37,7 @@ REL_NAME_CERTIFICATES = "certificates"
 
 
 @pytest.mark.abort_on_fail
+@pytest.mark.skip_if_deployed
 async def test_deploy_charms_relate_active(
     ops_test: OpsTest, kraft_mode, kafka_charm, app_charm, kafka_apps, usernames: set[str]
 ):
@@ -64,16 +66,19 @@ async def test_deploy_charms_relate_active(
 
     usernames.update(get_client_usernames(ops_test))
 
-    for username in usernames:
+    for username in set(usernames) - set(INTERNAL_USERS):
         check_user(
             username=username,
             model_full_name=ops_test.model_full_name,
         )
 
-    zk_data = get_kafka_zk_relation_data(ops_test=ops_test, owner=ZK, unit_name=f"{APP_NAME}/0")
-    zk_uris = zk_data.get("uris", "").split("/")[0]
+    address = await get_address(ops_test)
+    port = SECURITY_PROTOCOL_PORTS["SASL_PLAINTEXT", "SCRAM-SHA-512"].client
+    bootstrap_server = f"{address}:{port}"
 
-    for acl in load_acls(model_full_name=ops_test.model_full_name, zk_uris=zk_uris):
+    for acl in load_acls(
+        model_full_name=ops_test.model_full_name, bootstrap_server=bootstrap_server
+    ):
         assert acl.username in usernames
         assert acl.operation in ["READ", "DESCRIBE"]
         assert acl.resource_type in ["GROUP", "TOPIC"]
@@ -85,7 +90,7 @@ async def test_deploy_charms_relate_active(
 
 @pytest.mark.abort_on_fail
 async def test_deploy_multiple_charms_same_topic_relate_active(
-    ops_test: OpsTest, app_charm, usernames: set[str]
+    ops_test: OpsTest, app_charm, kafka_apps, usernames: set[str]
 ):
     """Test relation with multiple applications."""
     await ops_test.model.deploy(app_charm, application_name=DUMMY_NAME_2, num_units=1)
@@ -93,20 +98,23 @@ async def test_deploy_multiple_charms_same_topic_relate_active(
 
     async with ops_test.fast_forward(fast_interval="60s"):
         await ops_test.model.wait_for_idle(
-            apps=[APP_NAME, DUMMY_NAME_1, ZK], idle_period=30, status="active"
+            apps=[*kafka_apps, DUMMY_NAME_1], idle_period=60, status="active"
         )
 
     usernames.update(get_client_usernames(ops_test))
-    for username in usernames:
+    for username in set(usernames) - set(INTERNAL_USERS):
         check_user(
             username=username,
             model_full_name=ops_test.model_full_name,
         )
 
-    zk_data = get_kafka_zk_relation_data(ops_test=ops_test, owner=ZK, unit_name=f"{APP_NAME}/0")
-    zk_uris = zk_data.get("uris", "").split("/")[0]
+    address = await get_address(ops_test)
+    port = SECURITY_PROTOCOL_PORTS["SASL_PLAINTEXT", "SCRAM-SHA-512"].client
+    bootstrap_server = f"{address}:{port}"
 
-    for acl in load_acls(model_full_name=ops_test.model_full_name, zk_uris=zk_uris):
+    for acl in load_acls(
+        model_full_name=ops_test.model_full_name, bootstrap_server=bootstrap_server
+    ):
         assert acl.username in usernames
         assert acl.operation in ["READ", "DESCRIBE"]
         assert acl.resource_type in ["GROUP", "TOPIC"]
@@ -115,18 +123,21 @@ async def test_deploy_multiple_charms_same_topic_relate_active(
 
 
 @pytest.mark.abort_on_fail
-async def test_remove_application_removes_user_and_acls(ops_test: OpsTest, usernames: set[str]):
+async def test_remove_application_removes_user_and_acls(
+    ops_test: OpsTest, kafka_apps, usernames: set[str]
+):
     """Test the correct removal of user and permission after relation removal."""
     await ops_test.model.remove_application(DUMMY_NAME_1, block_until_done=True)
 
     async with ops_test.fast_forward(fast_interval="60s"):
-        await ops_test.model.wait_for_idle(apps=[APP_NAME, ZK], idle_period=30, status="active")
+        await ops_test.model.wait_for_idle(apps=kafka_apps, idle_period=60, status="active")
 
     # checks that old users are removed from active cluster ACLs
-    zk_data = get_kafka_zk_relation_data(ops_test=ops_test, owner=ZK, unit_name=f"{APP_NAME}/0")
-    zk_uris = zk_data.get("uris", "").split("/")[0]
+    address = await get_address(ops_test)
+    port = SECURITY_PROTOCOL_PORTS["SASL_PLAINTEXT", "SCRAM-SHA-512"].client
+    bootstrap_server = f"{address}:{port}"
 
-    acls = load_acls(model_full_name=ops_test.model_full_name, zk_uris=zk_uris)
+    acls = load_acls(model_full_name=ops_test.model_full_name, bootstrap_server=bootstrap_server)
     acl_usernames = set()
     for acl in acls:
         acl_usernames.add(acl.username)
@@ -143,7 +154,9 @@ async def test_remove_application_removes_user_and_acls(ops_test: OpsTest, usern
 
 
 @pytest.mark.abort_on_fail
-async def test_deploy_producer_same_topic(ops_test: OpsTest, app_charm, usernames: set[str]):
+async def test_deploy_producer_same_topic(
+    ops_test: OpsTest, app_charm, kafka_apps, usernames: set[str]
+):
     """Test the correct deployment and relation with role producer."""
     await asyncio.gather(
         ops_test.model.deploy(
@@ -154,13 +167,14 @@ async def test_deploy_producer_same_topic(ops_test: OpsTest, app_charm, username
 
     async with ops_test.fast_forward(fast_interval="60s"):
         await ops_test.model.wait_for_idle(
-            apps=[APP_NAME, DUMMY_NAME_1, ZK], idle_period=30, status="active"
+            apps=[*kafka_apps, DUMMY_NAME_1], idle_period=60, status="active"
         )
 
-    zk_data = get_kafka_zk_relation_data(ops_test=ops_test, owner=ZK, unit_name=f"{APP_NAME}/0")
-    zk_uris = zk_data.get("uris", "").split("/")[0]
+    address = await get_address(ops_test)
+    port = SECURITY_PROTOCOL_PORTS["SASL_PLAINTEXT", "SCRAM-SHA-512"].client
+    bootstrap_server = f"{address}:{port}"
 
-    acls = load_acls(model_full_name=ops_test.model_full_name, zk_uris=zk_uris)
+    acls = load_acls(model_full_name=ops_test.model_full_name, bootstrap_server=bootstrap_server)
     acl_usernames = set()
     for acl in acls:
         acl_usernames.add(acl.username)
@@ -176,14 +190,14 @@ async def test_deploy_producer_same_topic(ops_test: OpsTest, app_charm, username
 
     # remove application
     await ops_test.model.remove_application(DUMMY_NAME_1, block_until_done=True)
-    await ops_test.model.wait_for_idle(apps=[APP_NAME], idle_period=30, status="active")
+    await ops_test.model.wait_for_idle(apps=kafka_apps, idle_period=30, status="active")
 
 
 @pytest.mark.abort_on_fail
-async def test_admin_added_to_super_users(ops_test: OpsTest):
+async def test_admin_added_to_super_users(ops_test: OpsTest, kafka_apps):
     """Test relation with admin privileges."""
     super_users = load_super_users(model_full_name=ops_test.model_full_name)
-    assert len(super_users) == 2
+    assert len(super_users) == 3  # controller, sync, admin
 
     app_charm = await ops_test.build_charm("tests/integration/app-charm")
 
@@ -192,37 +206,39 @@ async def test_admin_added_to_super_users(ops_test: OpsTest):
             app_charm, application_name=DUMMY_NAME_1, num_units=1, series="jammy"
         )
     )
-    await ops_test.model.wait_for_idle(apps=[APP_NAME, DUMMY_NAME_1, ZK])
+    await ops_test.model.wait_for_idle(apps=[*kafka_apps, DUMMY_NAME_1])
     await ops_test.model.add_relation(APP_NAME, f"{DUMMY_NAME_1}:{REL_NAME_ADMIN}")
-    await ops_test.model.wait_for_idle(apps=[APP_NAME, DUMMY_NAME_1])
+    await ops_test.model.wait_for_idle(
+        apps=[*kafka_apps, DUMMY_NAME_1], status="active", idle_period=60
+    )
 
     assert ops_test.model.applications[APP_NAME].status == "active"
     assert ops_test.model.applications[DUMMY_NAME_1].status == "active"
 
     # check the correct addition of super-users
     super_users = load_super_users(model_full_name=ops_test.model_full_name)
-    assert len(super_users) == 3
+    assert len(super_users) == 4
 
 
 @pytest.mark.abort_on_fail
-async def test_admin_removed_from_super_users(ops_test: OpsTest):
+async def test_admin_removed_from_super_users(ops_test: OpsTest, kafka_apps):
     """Test that removal of the relation with admin privileges."""
     await ops_test.model.remove_application(DUMMY_NAME_1, block_until_done=True)
-    await ops_test.model.wait_for_idle(apps=[APP_NAME])
+    await ops_test.model.wait_for_idle(apps=kafka_apps)
     assert ops_test.model.applications[APP_NAME].status == "active"
 
-    await ops_test.model.wait_for_idle(apps=[APP_NAME, DUMMY_NAME_2])
+    await ops_test.model.wait_for_idle(apps=[*kafka_apps, DUMMY_NAME_2])
     assert ops_test.model.applications[APP_NAME].status == "active"
 
     super_users = load_super_users(model_full_name=ops_test.model_full_name)
-    assert len(super_users) == 2
+    assert len(super_users) == 3
 
     # adding cleanup to save memory
     await ops_test.model.remove_application(DUMMY_NAME_2, block_until_done=True)
 
 
 @pytest.mark.abort_on_fail
-async def test_prefixed_topic_creation(ops_test: OpsTest, app_charm):
+async def test_prefixed_topic_creation(ops_test: OpsTest, app_charm, kafka_apps):
     await asyncio.gather(
         ops_test.model.deploy(
             app_charm,
@@ -236,7 +252,7 @@ async def test_prefixed_topic_creation(ops_test: OpsTest, app_charm):
 
     async with ops_test.fast_forward(fast_interval="60s"):
         await ops_test.model.wait_for_idle(
-            apps=[APP_NAME, DUMMY_NAME_3, ZK], idle_period=30, status="active"
+            apps=[*kafka_apps, DUMMY_NAME_3], idle_period=30, status="active"
         )
 
     action = await ops_test.model.units.get(f"{DUMMY_NAME_3}/0").run_action("create-topic")
@@ -246,13 +262,15 @@ async def test_prefixed_topic_creation(ops_test: OpsTest, app_charm):
 
 
 @pytest.mark.abort_on_fail
-async def test_connection_updated_on_tls_enabled(ops_test: OpsTest, app_charm):
+async def test_connection_updated_on_tls_enabled(ops_test: OpsTest, app_charm, kafka_apps):
     """Test relation when TLS is enabled."""
     # adding new app unit to validate
     await ops_test.model.deploy(app_charm, application_name=DUMMY_NAME_1, num_units=1)
     await ops_test.model.wait_for_idle(apps=[DUMMY_NAME_1])
     await ops_test.model.add_relation(APP_NAME, f"{DUMMY_NAME_1}:{REL_NAME_CONSUMER}")
-    await ops_test.model.wait_for_idle(apps=[APP_NAME, DUMMY_NAME_1])
+    await ops_test.model.wait_for_idle(
+        apps=[*kafka_apps, DUMMY_NAME_1], status="active", idle_period=60
+    )
 
     # deploying tls
     tls_config = {"ca-common-name": "kafka"}
@@ -264,16 +282,14 @@ async def test_connection_updated_on_tls_enabled(ops_test: OpsTest, app_charm):
         apps=[TLS_NAME], idle_period=30, timeout=1800, status="active"
     )
 
-    # relating tls with zookeeper
-    await ops_test.model.add_relation(TLS_NAME, ZK)
-    await ops_test.model.wait_for_idle(
-        apps=[ZK, TLS_NAME], idle_period=60, timeout=1800, status="active"
-    )
-
     # relating tls with kafka
     await ops_test.model.add_relation(TLS_NAME, f"{APP_NAME}:{REL_NAME_CERTIFICATES}")
     await ops_test.model.wait_for_idle(
-        apps=[APP_NAME, ZK, TLS_NAME, DUMMY_NAME_1], timeout=1800, idle_period=60, status="active"
+        apps=[*kafka_apps, TLS_NAME, DUMMY_NAME_1],
+        timeout=1800,
+        idle_period=60,
+        status="active",
+        raise_on_error=False,
     )
 
     # ensure at least one update-status run
@@ -290,6 +306,5 @@ async def test_connection_updated_on_tls_enabled(ops_test: OpsTest, app_charm):
 
     assert provider_data["tls"] == "enabled"
     assert "9093" in provider_data["endpoints"]
-    assert "2182" in provider_data["zookeeper-uris"]
     assert "test-prefix" in provider_data["consumer-group-prefix"]
     assert "test-topic" in provider_data["topic"]
