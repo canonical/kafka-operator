c6a1b7a9ca3e5f960074644b87436cbb82ea1338
Raul Zamora <raul.zamora@canonical.com>
2025-07-08 10:02:40 +0200

    add PR feedback

--- tests/unit/test_ssl_principal_mapping.py
+++ tests/unit/test_ssl_principal_mapping.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python3
-# Copyright 2023 Canonical Ltd.
+# Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
+
+"""Test cases from Apache Kafka upstream for SslPrincipalMapperTest.java."""
 
 import logging
 
@@ -10,39 +12,6 @@ from managers.ssl_principal_mapper import SslPrincipalMapper
 
 logger = logging.getLogger(__name__)
 pytestmark = pytest.mark.broker
-
-
-def test_valid_rules():
-    _test_valid_rule("DEFAULT")
-    _test_valid_rule("RULE:^CN=(.*?),OU=ServiceUsers.*$/$1/")
-    _test_valid_rule("RULE:^CN=(.*?),OU=ServiceUsers.*$/$1/L, DEFAULT")
-    _test_valid_rule("RULE:^CN=(.*?),OU=ServiceUsers.*$/$1@$2/")
-    _test_valid_rule("RULE:^.*[Cc][Nn]=([a-zA-Z0-9.]*).*$/$1/L")
-    _test_valid_rule("RULE:^cn=(.?),ou=(.?),dc=(.?),dc=(.?)$/$1@$2/U")
-    _test_valid_rule("RULE:^CN=([^,ADEFLTU,]+)(,.*|$)/$1/")
-    _test_valid_rule("RULE:^CN=([^,DEFAULT,]+)(,.*|$)/$1/")
-
-
-def _test_valid_rule(rules):
-    SslPrincipalMapper.parse_rules(SslPrincipalMapper.split_rules(rules))
-
-
-def test_invalid_rules():
-    invalid_rules = [
-        "default",
-        "DEFAUL",
-        "DEFAULT/L",
-        "DEFAULT/U",
-        "RULE:CN=(.*?),OU=ServiceUsers.*/$1",
-        "rule:^CN=(.*?),OU=ServiceUsers.*$/$1/",
-        "RULE:^CN=(.*?),OU=ServiceUsers.*$/$1/L/U",
-        "RULE:^CN=(.*?),OU=ServiceUsers.*$/L",
-        "RULE:^CN=(.*?),OU=ServiceUsers.*$/U",
-        "RULE:^CN=(.*?),OU=ServiceUsers.*$/LU",
-    ]
-    for rules in invalid_rules:
-        with pytest.raises(ValueError):
-            SslPrincipalMapper.parse_rules(SslPrincipalMapper.split_rules(rules))
 
 
 def test_ssl_principal_mapper():
@@ -65,37 +34,74 @@ def test_ssl_principal_mapper():
     )
 
 
-def _test_rules_splitting(expected, rules):
+@pytest.mark.parametrize(
+    "rules",
+    [
+        "DEFAULT",
+        "RULE:^CN=(.*?),OU=ServiceUsers.*$/$1/",
+        "RULE:^CN=(.*?),OU=ServiceUsers.*$/$1/L, DEFAULT",
+        "RULE:^CN=(.*?),OU=ServiceUsers.*$/$1@$2/",
+        "RULE:^.*[Cc][Nn]=([a-zA-Z0-9.]*).*$/$1/L",
+        "RULE:^cn=(.?),ou=(.?),dc=(.?),dc=(.?)$/$1@$2/U",
+        "RULE:^CN=([^,ADEFLTU,]+)(,.*|$)/$1/",
+        "RULE:^CN=([^,DEFAULT,]+)(,.*|$)/$1/",
+    ],
+)
+def test_valid_rules(rules):
+    SslPrincipalMapper.parse_rules(SslPrincipalMapper.split_rules(rules))
+
+
+@pytest.mark.parametrize(
+    "rules",
+    [
+        "default",
+        "DEFAUL",
+        "DEFAULT/L",
+        "DEFAULT/U",
+        "RULE:CN=(.*?),OU=ServiceUsers.*/$1",
+        "rule:^CN=(.*?),OU=ServiceUsers.*$/$1/",
+        "RULE:^CN=(.*?),OU=ServiceUsers.*$/$1/L/U",
+        "RULE:^CN=(.*?),OU=ServiceUsers.*$/L",
+        "RULE:^CN=(.*?),OU=ServiceUsers.*$/U",
+        "RULE:^CN=(.*?),OU=ServiceUsers.*$/LU",
+    ],
+)
+def test_invalid_rules(rules):
+    with pytest.raises(ValueError):
+        SslPrincipalMapper.parse_rules(SslPrincipalMapper.split_rules(rules))
+
+
+@pytest.mark.parametrize(
+    "expected, rules",
+    [
+        ("[]", ""),
+        ("[DEFAULT]", "DEFAULT"),
+        ("[RULE:/]", "RULE://"),
+        ("[RULE:/.*]", "RULE:/.*/"),
+        ("[RULE:/.*/L]", "RULE:/.*/L"),
+        ("[RULE:/, DEFAULT]", "RULE://,DEFAULT"),
+        ("[RULE:/, DEFAULT]", "  RULE:// ,  DEFAULT  "),
+        ("[RULE:   /     , DEFAULT]", "  RULE:   /     / ,  DEFAULT  "),
+        ("[RULE:  /     /U, DEFAULT]", "  RULE:  /     /U   ,DEFAULT  "),
+        (
+            "[RULE:([A-Z]*)/$1/U, RULE:([a-z]+)/$1, DEFAULT]",
+            "  RULE:([A-Z]*)/$1/U   ,RULE:([a-z]+)/$1/,   DEFAULT  ",
+        ),
+        ("[]", ",   , , ,      , , ,   "),
+        ("[RULE:/, DEFAULT]", ",,RULE://,,,DEFAULT,,"),
+        ("[RULE: /   , DEFAULT]", ",  , RULE: /   /    ,,,   DEFAULT, ,   "),
+        ("[RULE:   /  /U, DEFAULT]", "     ,  , RULE:   /  /U    ,,  ,DEFAULT, ,"),
+        ("[RULE:\\/\\\\\\(\\)\\n\\t/\\/\\/]", "RULE:\\/\\\\\\(\\)\\n\\t/\\/\\//"),
+        ("[RULE:\\**\\/+/*/L, RULE:\\/*\\**/**]", "RULE:\\**\\/+/*/L,RULE:\\/*\\**/**/"),
+        (
+            "[RULE:,RULE:,/,RULE:,\\//U, RULE:,/RULE:,, RULE:,RULE:,/L,RULE:,/L, RULE:, DEFAULT, /DEFAULT, DEFAULT]",
+            "RULE:,RULE:,/,RULE:,\\//U,RULE:,/RULE:,/,RULE:,RULE:,/L,RULE:,/L,RULE:, DEFAULT, /DEFAULT/,DEFAULT",
+        ),
+    ],
+)
+def test_rules_splitting(expected, rules):
     mapper = SslPrincipalMapper(rules)
     assert f"SslPrincipalMapper(rules = {expected})" == str(mapper)
-
-
-def test_rules_splitting():
-    _test_rules_splitting("[]", "")
-    _test_rules_splitting("[DEFAULT]", "DEFAULT")
-    _test_rules_splitting("[RULE:/]", "RULE://")
-    _test_rules_splitting("[RULE:/.*]", "RULE:/.*/")
-    _test_rules_splitting("[RULE:/.*/L]", "RULE:/.*/L")
-    _test_rules_splitting("[RULE:/, DEFAULT]", "RULE://,DEFAULT")
-    _test_rules_splitting("[RULE:/, DEFAULT]", "  RULE:// ,  DEFAULT  ")
-    _test_rules_splitting("[RULE:   /     , DEFAULT]", "  RULE:   /     / ,  DEFAULT  ")
-    _test_rules_splitting("[RULE:  /     /U, DEFAULT]", "  RULE:  /     /U   ,DEFAULT  ")
-    _test_rules_splitting(
-        "[RULE:([A-Z]*)/$1/U, RULE:([a-z]+)/$1, DEFAULT]",
-        "  RULE:([A-Z]*)/$1/U   ,RULE:([a-z]+)/$1/,   DEFAULT  ",
-    )
-    _test_rules_splitting("[]", ",   , , ,      , , ,   ")
-    _test_rules_splitting("[RULE:/, DEFAULT]", ",,RULE://,,,DEFAULT,,")
-    _test_rules_splitting("[RULE: /   , DEFAULT]", ",  , RULE: /   /    ,,,   DEFAULT, ,   ")
-    _test_rules_splitting("[RULE:   /  /U, DEFAULT]", "     ,  , RULE:   /  /U    ,,  ,DEFAULT, ,")
-    _test_rules_splitting("[RULE:\\/\\\\\\(\\)\\n\\t/\\/\\/]", "RULE:\\/\\\\\\(\\)\\n\\t/\\/\\//")
-    _test_rules_splitting(
-        "[RULE:\\**\\/+/*/L, RULE:\\/*\\**/**]", "RULE:\\**\\/+/*/L,RULE:\\/*\\**/**/"
-    )
-    _test_rules_splitting(
-        "[RULE:,RULE:,/,RULE:,\\//U, RULE:,/RULE:,, RULE:,RULE:,/L,RULE:,/L, RULE:, DEFAULT, /DEFAULT, DEFAULT]",
-        "RULE:,RULE:,/,RULE:,\\//U,RULE:,/RULE:,/,RULE:,RULE:,/L,RULE:,/L,RULE:, DEFAULT, /DEFAULT/,DEFAULT",
-    )
 
 
 def test_comma_with_whitespace():
