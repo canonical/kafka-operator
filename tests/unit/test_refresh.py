ceafd2316a37410d677af0148f1f808999d46eb2
Iman Enami <iman.enami@canonical.com>
2025-09-22 11:56:27 +0400

    [DPE-8312] feat: auto-balance

--- tests/unit/test_refresh.py
+++ tests/unit/test_refresh.py
@@ -2,6 +2,7 @@
 # Copyright 2025 Canonical Ltd.
 # See LICENSE file for licensing details.
 
+import dataclasses
 import logging
 from pathlib import Path
 from typing import cast
@@ -58,7 +59,7 @@ def test_is_workload_compatible(old_version: str, new_version: str, expected: bo
 @pytest.mark.skipif(SUBSTRATE == "k8s", reason="snap_refresh not applicable in k8s")
 def test_post_snap_refresh_healthy_cluster(ctx: Context, base_state: State) -> None:
     # Given
-    state_in = base_state
+    state_in = dataclasses.replace(base_state, config=base_state.config | {"auto-balance": False})
 
     # When
     with (ctx(ctx.on.config_changed(), state_in) as manager,):
