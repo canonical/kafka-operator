86bdaea31112dd9b131371aad5421a26aa34b31c
Iman Enami <iman.enami@canonical.com>
2025-10-16 10:11:56 +0200

    [DPE-8529] feat: add provider support for multiple requests and permission requests

--- src/events/broker.py
+++ src/events/broker.py
@@ -256,8 +256,9 @@ class BrokerOperator(Object):
         # Update IP addresses based on current network bindings.
         self.update_ip_addresses()
 
+        # The order is important here, first update the credentials cache,
+        # then the client relation data.
         self.update_credentials_cache()
-
         self.provider.update_client_data()
 
         if self.charm.state.peer_cluster_orchestrator_relation and self.charm.unit.is_leader():
