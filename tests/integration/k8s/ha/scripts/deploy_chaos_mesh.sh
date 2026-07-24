401db5130587b4061eaf8b4a2ff7f7e8b475e7ea
Iman Enami <iman.enami@canonical.com>
2026-07-24 11:33:21 +0400

    test fixes & stabilization on CK8s

--- tests/integration/k8s/ha/scripts/deploy_chaos_mesh.sh
+++ tests/integration/k8s/ha/scripts/deploy_chaos_mesh.sh
@@ -10,15 +10,24 @@ if [ -z "${chaos_mesh_ns}" ]; then
 	exit 1
 fi
 
+# Check if microk8s is available
+if command -v microk8s.helm3 >/dev/null 2>&1; then
+  helm_cmd="microk8s.helm3"
+  socket_path=/var/snap/microk8s/common/run/containerd.sock
+else
+  helm_cmd="k8s helm"
+  socket_path=/run/containerd/containerd.sock
+fi
+
 deploy_chaos_mesh() {
 	echo "adding chaos-mesh helm repo"
-	microk8s.helm3 repo add chaos-mesh https://charts.chaos-mesh.org
+	$helm_cmd repo add chaos-mesh https://charts.chaos-mesh.org
 
 	echo "installing chaos-mesh"
-        microk8s.helm3 install chaos-mesh chaos-mesh/chaos-mesh \
+        $helm_cmd install chaos-mesh chaos-mesh/chaos-mesh \
           --namespace="${chaos_mesh_ns}" \
           --set chaosDaemon.runtime=containerd \
-          --set chaosDaemon.socketPath=/var/snap/microk8s/common/run/containerd.sock \
+          --set chaosDaemon.socketPath="$socket_path" \
           --set dashboard.create=false \
           --version "${chaos_mesh_version}" \
           --set clusterScoped=false \
